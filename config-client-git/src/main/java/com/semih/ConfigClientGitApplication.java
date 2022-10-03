a56ccfeea3d9ac3b836367f29a0c7b78ba371f93
Semih Biygit <semihbiygit@gmail.com>
2022-10-03 16:09:22 +0300

    config-server configuration with using git.

---
+++ config-client-git/src/main/java/com/semih/ConfigClientGitApplication.java
@@ -0,0 +1,11 @@
+package com.semih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientGitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientGitApplication.class, args);
+    }
+}
